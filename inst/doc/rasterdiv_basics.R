da1b3eeca85465853b8065510a9a17949eb42d6d
Matteo Marcantonio <marcantoniomatteo@gmail.com>
2020-07-18 04:20:02 +0000

    version 0.2-1

--- inst/doc/rasterdiv_basics.R
+++ inst/doc/rasterdiv_basics.R
@@ -1,58 +1,27 @@
----
-title: "Rasterdiv basics. Derive indices of diversity from NDVI."
-author: "Matteo Marcantonio, Duccio Rocchini"
-output: rmarkdown::html_vignette
-vignette: >
-  %\VignetteIndexEntry{Rasterdiv basics. Derive spatial indices of diversity from NDVI.}
-  %\VignetteEngine{knitr::rmarkdown}
-  \usepackage[utf8]{inputenc}
----
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::opts_chunk$set(fig.width=10, fig.height=10,fig.asp = 0.618, out.width = "95%", fig.align = "center", fig.dpi = 150, collapse = FALSE, comment = "#") 
+#knitr::opts_chunk$set(dev = 'pdf')
 
-```{r setup, include=FALSE}
-knitr::opts_chunk$set(fig.width=10, fig.height=10,fig.asp = 0.618, out.width = "95%", fig.align = "center") 
-knitr::opts_chunk$set(dev = 'pdf')
-```
-
-```{r results='hide', message=FALSE, warning=FALSE}
+## ----results='hide', message=FALSE, warning=FALSE-----------------------------
 #Required packages
-library(rasterdiv)
-library(rasterVis)
-library(RColorBrewer)
-```
+require(rasterdiv)
+require(rasterVis)
+require(RColorBrewer)
 
-This vignette uses **rasterdiv** to build global series of indices of diversity based on Information Theory. The input dataset is the Copernicus Long-term (1999-2017) average Normalised Difference Vegetation Index for the 21st of June (copNDVI).
+## -----------------------------------------------------------------------------
+copNDVI <- raster::reclassify(copNDVI, cbind(252,255, NA), right=TRUE)
 
-## Overview
-A RasterLayer called copNDVI is loaded together with the package **rasterdiv**. *copNDVI* is a 8-bit raster, meaning that pixel values range from 0 to 255. You could *stretch* it to match a more familiar (-1,1) values range using `raster::stretch(copNDVI,minv=-1,maxv=1)` .
-
-## Reclassify NDVI 
-Pixels with values 253, 254 and 255 (water) will be set as NA's.
-
-```{r}
-copNDVI <- reclassify(copNDVI, cbind(252,255, NA), right=TRUE)
-```
-
-## Resample NDVI to coarser resolution 
-To speed up the calculation, the RasterLayer will be "resampled" at a resolution 20 times coarser than original.
-
-```{r}
+## -----------------------------------------------------------------------------
 #Resample using raster::aggregate and a linear factor of 20
 copNDVIlr <- raster::aggregate(copNDVI, fact=20)
 #Set float numbers as integers to further speed up the calculation
 storage.mode(copNDVIlr[]) = "integer"
-```
 
-## Compare NDVI low and high resolution
-
-```{r fig01}
+## ----fig01--------------------------------------------------------------------
 levelplot(copNDVI,layout=c(0,1,1), main="NDVI 21st of June 1999-2017 - ~8km pixel resolution")
 levelplot(copNDVIlr,layout=c(0,1,1), main="NDVI 21st of June 1999-2017 - ~150km pixel resolution")
-```
 
-## Compute all indexes in rasterdiv
-**rasterdiv** allows the computation of 8 diversity indexes based on information theory. In the following section, all these indexes will be computed for *copNDVIlr* using a moving window of 81 pixels (9 px side). Alpha values for the Hill, Renyi and parametric Rao indexes will be set from 0 to 2 every 0.5. In addition, we will set `na.tolerance=0.1`, meaning that all moving windows with more than 10% of pixels equal NA will be set to NA.
-
-```{r echo = T, results = 'hide', warning=FALSE, message=FALSE}
+## ----echo = T, results = 'hide', warning=FALSE, message=FALSE-----------------
 #Shannon's Diversity
 sha <- Shannon(copNDVIlr,window=9,na.tolerance=0.1,np=1)
 
@@ -76,47 +45,36 @@ hil <- Hill(copNDVIlr,window=9,alpha=seq(0,2,0.5),na.tolerance=0.1,np=1)
 
 #Renyi's Index
 ren <- Renyi(copNDVIlr,window=9,alpha=seq(0,2,0.5),na.tolerance=0.1,np=1)
-```
 
-
-## Visualise RasterLayers
-
-```{r fig02}
+## ----fig02--------------------------------------------------------------------
 #Shannon's Diversity
 levelplot(sha,main="Shannon's entropy from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,1,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig03}
+## ----fig03--------------------------------------------------------------------
 #Pielou's Evenness
 levelplot(pie,main="Pielou's evenness from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,1,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig04}
+## ----fig04--------------------------------------------------------------------
 #Berger-Parker' Index
 levelplot(ber,main="Berger-Parker's index from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,1,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig05}
+## ----fig05--------------------------------------------------------------------
 #Rao's quadratic Entropy
 levelplot(rao,main="Rao's quadratic entropy from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,1,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig06}
+## ----fig06--------------------------------------------------------------------
 #Parametric Rao's quadratic Entropy
 levelplot(stack(prao),main="Parametric Rao's quadratic entropy from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,5,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig07}
+## ----fig07--------------------------------------------------------------------
 #Cumulative Residual Entropy
 levelplot(cre,main="Cumulative Residual Entropy from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,1,1), ylim=c(-60,75), margin = list(draw = TRUE))
-```
 
-```{r fig08}
+## ----fig08--------------------------------------------------------------------
 #Hill's numbers (alpha=0, 1, 1.5 and 2)
 levelplot(stack(hil),main="Hill's numbers from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,5,1),names.attr=paste("alpha",seq(0,2,0.5),sep=" "), ylim=c(-60,75))
-```
 
-```{r fig09}
+## ----fig09--------------------------------------------------------------------
 #Renyi' Index (alpha=0, 1, 1.5 and 2)
 levelplot(stack(ren),main="Renyi's entropy from Copernicus NDVI 5 km (9 px-side moving window)",as.table = T,layout=c(0,5,1),names.attr=paste("alpha",seq(0,2,0.5),sep=" "), ylim=c(-60,75), margin = list(draw = FALSE))
-```
+
